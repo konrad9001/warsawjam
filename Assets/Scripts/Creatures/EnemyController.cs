a8af5c52f425d398db58486a1b1c9412d0a51274
Karol Górski <goralk2870@gmail.com>
2018-11-10 02:47:28 +0100

    Many models, terrain, lighting. All on second scene. Before-merge

---
+++ Assets/Scripts/Creatures/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    List<BaseEnemy> listOfEnemies;
+
+    [SerializeField]
+    private GameObject dynamicParent;
+
+    void OnEnable()
+    {
+        listOfEnemies = new List<BaseEnemy>();
+    }
+	
+    
+}
